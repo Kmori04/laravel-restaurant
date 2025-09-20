76d5e65fbed81d0487d1fdb50bca54444795650a
Kmori04 <kenmori197@gmail.com>
2025-09-20 18:39:59 +0800

    Finish CRUD Function.md

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
 
-Route::get('/home', [CustomerController::class, 'showAllCustomers']);
+Route::get('/home', [CustomerController::class, 'showAllCustomers'])->name('home');
+
+Route::post('/saveCustomer', [CustomerController::class, 'saveCustomerDetails'])-> name ('saveCustomer') ;
+
+Route::get('/editCustomer/{cust_ID}',[CustomerController::class, 'ShowCustomerDetails']) -> name('customerEdit');
+
+Route::delete('/deleteCustomer/{cust_ID}',[CustomerController::class, 'deleteCustomerDetails']) -> name('customerDelete');
+
+Route::put('/saveEditCustomer/{cust_ID}', [CustomerController::class, 'saveEditCustomer']) ->name('saveEditCustomer');
